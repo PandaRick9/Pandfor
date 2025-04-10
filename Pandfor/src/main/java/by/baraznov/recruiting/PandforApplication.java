10c2ddacb33ad10017c1aad53247e1e08a69e323
derfect90 <herettyr765@gmail.com>
2025-04-10 20:03:43 +0300

    Start project

---
+++ Pandfor/src/main/java/by/baraznov/recruiting/PandforApplication.java
@@ -0,0 +1,13 @@
+package by.baraznov.recruiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandforApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandforApplication.class, args);
+    }
+
+}
